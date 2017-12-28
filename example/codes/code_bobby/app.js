d8dce2ad3abcb376ca453578360a5e2419957f0c
tongjie <tongjie@boco.com.cn>
2017-12-28 10:28:47 +0800

    demo提交一次

--- example/codes/code_bobby/app.js
+++ example/codes/code_bobby/app.js
@@ -5,19 +5,7 @@
 App({
   onLaunch: function () {
 
-
     
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -27,12 +15,14 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+            }
+          })
+        }
+        if (res.authSetting['scope.userLocation']) {
+          // 已经授权，可以直接调用 getLocation 获取位置信息，不会弹框
+          wx.getLocation({
+            success: res => {
+              this.globalData.location = res
             }
           })
         }
@@ -40,6 +30,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    location: null
   }
 })
--- example/codes/code_bobby/pages/index/index.js
+++ example/codes/code_bobby/pages/index/index.js
@@ -4,51 +4,110 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
+    weatherDailyInfo:{},
+    weatherNowInfo: {},
+    cityInfo:"",
+    suggestion:{},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    hasLocation: false,
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../location/location'
-    })
+  onPullDownRefresh: function(){
+    this.getWeather()
+  },
+  onReady:function(){
+    setInterval(this.getWeather, 2*60*1000);
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
+    if (this.data.hasUserInfo && this.data.hasLocation) {
+      //获取天气信息
+      this.getDailyWeather()
+      this.getNowWeather()
+    } else{
+      //获取位置信息
+      wx.getLocation({
         success: res => {
-          app.globalData.userInfo = res.userInfo
+          app.globalData.location = res
           this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+            hasLocation:true
           })
+          if (!this.data.hasUserInfo){
+            wx.getUserInfo({
+              success: res => {
+                app.globalData.userInfo = res.userInfo
+                this.setData({
+                  userInfo: res.userInfo,
+                  hasUserInfo: true
+                })
+                //这里请求一次api获取天气
+                this.getDailyWeather()
+                this.getNowWeather()
+              }
+            })
+          }
         }
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  getDailyWeather: function() {
+    //获取天气
+    var self =this;
+    wx.request({
+      url: "https://bobbycat.club/api/weatherDaily/",
+      data: app.globalData,
+      method: "POST",
+      success: function (res) {
+        var weather = res.data.results[0];
+        //这里不用this是因为，在自定义的函数里边调this是拿不到this的，，，，
+        self.setData({
+          weatherDailyInfo: weather.daily,
+        })
+      },
+      fail: function (res) {
+        console.log("额,请求失败了..")
+      }
+    })
+  },
+  getNowWeather:function(){
+    //获取现在的天气
+    var self = this;
+    wx.request({
+      url: "https://bobbycat.club/api/weathers/",
+      data: app.globalData,
+      method: "POST",
+      success: function (res) {
+        var weather = res.data.results[0];
+        self.setData({
+          weatherNowInfo: weather.now,
+          cityInfo: weather.location.name
+        })
+      },
+      fail: function (res) {
+        console.log("额,请求失败了..")
+      }
+    })
+  },
+  getWeather: function () {
+    //需要获取实时天气,位置信息是可变化的，需要最新的位置信息
+    var self = this;
+    wx.getLocation({
+      success: res => {
+        app.globalData.location = res
+        wx.request({
+          url: "https://bobbycat.club/api/weathers/",
+          data: app.globalData,
+          method: "POST",
+          success: function (res) {
+            var weather = res.data.results[0];
+            self.setData({
+              weatherNowInfo: weather.now,
+              cityInfo: weather.location.name
+            })
+          },
+          fail: function (res) {
+            console.log("额,请求失败了..")
+          }
+        })
+      }
     })
   }
 })
